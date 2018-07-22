bcf939f3e3fc803dfb56ba0508c834c06fc64f7d
Michael <michael.fitzhavey@gmail.com>
2018-07-22 01:46:16 +0100

    big tidy

--- src/containers/Landing/Page.vue
+++ src/containers/Landing/Page.vue
@@ -1,48 +1,30 @@
 
 <template>
-	<div class="hello">
+	<div class="Landing">
 		<div class="App__inner">
 
+			<b-link href="#/games">
+				<b-card
+					class="Landing__card"
+					overlay
+					img-src="/static/images/games.jpg"
+					img-alt="Games"
+					text-variant="white"
+					title="Games"/>
+			</b-link>
 
 		</div>
 	</div>
 </template>
 
-<script>
-
-export default {
-	name: 'HelloWorld',
-	data() {
-		return {
-			msg: 'Welcome to Your Vue.js App'
-		};
-	},
-	computed: {
-
-	},
-	methods: {
-
-	},
-	created() {
-	}
-};
-</script>
-
 <style lang="scss">
 @import '../../settings';
 
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.Landing {
+
+	&__card {
+		height: 270px;
+		overflow: hidden;
+	}
 }
 </style>
